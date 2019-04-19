a27f07786cc62958642c91e4c128f97ca27330fc
Rudra_czs <mokarrom.alam@northsouth.edu>
2019-04-19 13:46:45 +0600

    Create user_profile.php
    search action will show here

---
+++ code/user_profile.php
@@ -0,0 +1,18 @@
+<?php
+
+  include "header.php";
+
+?>
+
+     
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+
+    <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.js"></script>
+    <script type="text/javascript" src="js/app.js"></script>
+</body>
+
+</html>
